d047a0b4e066ba8bf0c2188855b39459d05de3b6
immohammedbilal98 <im.mohammedbilal98@gmail.com>
2022-01-07 18:41:36 +0400

    make it better

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,20 @@
 //Change Navbar Scrolling 
-window.addEventListener('scroll', function () {
-    let header = document.querySelector('header');
-    // let nav_text = document.querySelectorAll('a.nav-link')
-    let windowPosition = window.scrollY > 0;
+// window.addEventListener('scroll', function () {
+//     let header = document.querySelector('header');
 
-    header.classList.toggle('scrolling-active', windowPosition);
-    // nav_text.classList.toggle('scrolling-active-nav-text', windowPosition);
-})
+//     let windowPosition = window.scrollY > 0;
+
+//     header.classList.toggle('scrolling-active', windowPosition);
+    
+// })
 
 
 //Hamburger Button Animation
 let ChangeIcon = function(icon){
     icon.classList.toggle("change")
 }
+
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+  });
