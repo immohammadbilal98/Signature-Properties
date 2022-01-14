71bf4d386aa4d67a65350c67c8f815153ded8262
immohammedbilal98 <im.mohammedbilal98@gmail.com>
2022-01-14 18:18:37 +0400

    make it better

--- public/js/script.js
+++ public/js/script.js
@@ -9,6 +9,7 @@ window.addEventListener('scroll', function () {
  
 })*/
 
+
 // Hamburger Button Animation
 let ChangeIcon = function(icon){
     icon.classList.toggle("change")
@@ -26,6 +27,61 @@ for(let i=0; i<marqueeElementsDisplayed; i++) {
 }
 
 
+//Gallery Filter
+
+//selecting all required elements
+const filterItem = document.querySelector(".items-gallery");
+const filterImg = document.querySelectorAll(".gallery .image");
+window.onload = ()=>{ //after window loaded
+  filterItem.onclick = (selectedItem)=>{ //if user click on filterItem div
+    if(selectedItem.target.classList.contains("item-gallery")){ //if user selected item has .item class
+      filterItem.querySelector(".active").classList.remove("active"); //remove the active class which is in first item
+      selectedItem.target.classList.add("active"); //add that active class on user selected item
+      let filterName = selectedItem.target.getAttribute("data-name"); //getting data-name value of user selected item and store in a filtername variable
+      filterImg.forEach((image) => {
+        let filterImges = image.getAttribute("data-name"); //getting image data-name value
+        //if user selected item data-name value is equal to images data-name value
+        //or user selected item data-name value is equal to "all"
+        if((filterImges == filterName) || (filterName == "all")){
+          image.classList.remove("hide"); //first remove the hide class from the image
+          image.classList.add("show"); //add show class in image
+        }else{
+          image.classList.add("hide"); //add hide class in image
+          image.classList.remove("show"); //remove show class from the image
+        }
+      });
+    }
+  }
+  /*for (let i = 0; i < filterImg.length; i++) {
+    filterImg[i].setAttribute("onclick", "preview(this)"); //adding onclick attribute in all available images
+  }*/
+}
+
+/*
+//fullscreen image preview function
+//selecting all required elements
+const previewBox = document.querySelector(".preview-box"),
+categoryName = previewBox.querySelector(".title p"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon"),
+shadow = document.querySelector(".shadow");
+function preview(element){
+  //once user click on any image then remove the scroll bar of the body, so user cant scroll up or down
+  document.querySelector("body").style.overflow = "hidden";
+  let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
+  let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
+  previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source
+  categoryName.textContent = selectedImgCategory; //passing user clicked data-name value in category name
+  previewBox.classList.add("show"); //show the preview image box
+  shadow.classList.add("show"); //show the light grey background
+  closeIcon.onclick = ()=>{ //if user click on close icon of preview box
+    previewBox.classList.remove("show"); //hide the preview box
+    shadow.classList.remove("show"); //hide the light grey background
+    document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
+  }
+}
+*/
+
 // JQUERY ===============================================================
 
 // Tooltips 
@@ -37,3 +93,98 @@ $(document).ready(function(){
 
 
 
+let products = rawdata;
+
+// show all the products when we load the page
+showData(products);
+
+// getter and setter of current products collection
+function getCurrentProducts() {
+    return products;
+}
+
+function setCurrentProducts(pros) {
+    products = pros;
+}
+
+// whole filter function
+function showWithFilter() {
+    setCurrentProducts(rawdata);
+    let proWithCate = filterCategory(getCurrentProducts(), document.getElementById('category').value);
+    let prowithFilter = filterPrice(proWithCate, document.getElementById('price').value);
+    showData(prowithFilter);
+    setCurrentProducts(prowithFilter);
+}
+
+// category filter
+function filterCategory (productsArray, cateId) {
+    return productsArray.filter(product => (product.categoryId == cateId || cateId == "0"));
+}
+
+// price filter
+function filterPrice (productsArray, priceFilter) {
+    let priceProducts;
+    switch (priceFilter) {
+        case "0":
+            priceProducts = productsArray;
+            break;
+        case "100":
+            priceProducts = productsArray.filter(product => 
+                product.price <= 100);
+            break;
+        case "500":
+            priceProducts = productsArray.filter(product => 
+                (product.price > 100 && product.price <= 500));
+            break;
+        case "1000":
+            priceProducts = productsArray.filter(product => 
+                (product.price > 500 && product.price <= 1000));
+            break;
+        case "1001":
+            priceProducts = productsArray.filter(product => 
+                product.price > 1000);
+            break;
+        default:
+            break;
+    }
+    return priceProducts;
+}
+
+// sorting products ascending
+function productsAsc() {
+    let ascProducts = getCurrentProducts().sort((a, b) => a.price - b.price);
+    showData(ascProducts);
+}
+
+// sorting products descending
+function productsDesc() {
+    let descProducts = getCurrentProducts().sort((a, b) => b.price - a.price);
+    showData(descProducts);
+}
+
+// reset products to origin
+function resetFilter() {
+    document.getElementById('category').value = "0";
+    document.getElementById('price').value = "0";
+    showData(rawdata);
+}
+
+function showData (arrays) {
+    let productsToAdd = '';
+    arrays.forEach(product => {
+        if (product.productMedia[0] && product.productMedia[0].url) {
+            let imgUrl = "https://storage.googleapis.com/luxe_media/wwwroot/" + product.productMedia[0].url;
+            let urlParams = "./detail.html?prodId=" + product.prodId + "&prodTitle=" + product.title;
+
+            productsToAdd += `
+                <div class="col-lg-2 col-md-4 col-sm-12 mb-3">
+                    <a href="${urlParams}" style="color: grey;">
+                        <img src="${imgUrl}" style="width: 100%; height: 200px; display: block; border-radius: 5px;">
+                        <p style="width: 90%; height: 35px">${product.title}</p>
+                        <p>$ ${product.price}</p>
+                    </a>
+                </div>`;
+        }
+    });
+    document.getElementById('display').innerHTML = productsToAdd;
+}
